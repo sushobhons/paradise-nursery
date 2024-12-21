aa234b73771e75eb5d155df4e6b4bdcc63bd065e
Sushobhon Sahoo <sushobhons@gmail.com>
2024-12-21 09:27:36 -0500

    final commit

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -22,6 +22,10 @@ const CartItem = ({ onContinueShopping, onRemoveFromCart }) => {
         onContinueShopping(e);
     };
 
+    const handleCheckoutShopping = (e) => {
+        alert('Functionality to be added for future reference');
+    };
+
     const handleIncrement = (item) => {
         dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
     };
@@ -69,7 +73,7 @@ const CartItem = ({ onContinueShopping, onRemoveFromCart }) => {
             <div className="continue_shopping_btn">
                 <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
                 <br />
-                <button className="get-started-button1">Checkout</button>
+                <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
             </div>
         </div>
     );
